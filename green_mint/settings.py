dc09cb5a9a2a8750e791e19a94a3421264d9239b
Susheel <sy93550@gmail.com>
2024-09-03 17:17:51 +0530

    deploy

--- green_mint/settings.py
+++ green_mint/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-2zcwxsjae-glpnf%dinnog@z(m5ne)%fisyti@#9url&=bow(y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['64.227.158.56']
+
+CSRF_TRUSTED_ORIGINS = ['64.227.158.56']
 
 
 # Application definition
